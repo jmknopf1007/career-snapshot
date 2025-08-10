fba5ff79b025da35edecc8e2b8c8c2cff95befdb
Jacob Knopf <jacobknopf@Jacobs-MacBook-Air.local>
2025-08-10 12:07:15 -0500

    Fixing Safari and Chrome mobile scaling issues

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,21 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-import { useEffect } from "react";
+import '@/styles/globals.css'
+import 'react-notion-x/src/styles.css'
+import 'prismjs/themes/prism-tomorrow.css'
+import 'katex/dist/katex.min.css'
+import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
     function setVhProperty() {
-      const vh = window.innerHeight * 0.01;
-      document.documentElement.style.setProperty("--vh", `${vh}px`);
+      const vh = window.innerHeight * 0.01
+      document.documentElement.style.setProperty('--vh', `${vh}px`)
     }
-    setVhProperty();
-    window.addEventListener("resize", setVhProperty);
-    return () => {
-      window.removeEventListener("resize", setVhProperty);
-    };
-  }, []);
+    setVhProperty()
+    window.addEventListener('resize', setVhProperty)
+    return () => window.removeEventListener('resize', setVhProperty)
+  }, [])
 
-  return <Component {...pageProps} />;
+  return <Component {...pageProps} />
 }
+
