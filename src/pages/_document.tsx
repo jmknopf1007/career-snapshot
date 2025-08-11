7f3e6da624f94bb00d22fd0e48eebc6a8c790293
Jacob Knopf <jacobknopf@Jacobs-MacBook-Air.local>
2025-08-11 11:11:41 -0500

    Trying to update meta tag to use 390 screen width instead of device width for Safari mobile issue

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,8 +7,8 @@ export default function Document() {
         {/* Viewport with user-scalable=no to prevent zoom */}
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no"
-        />
+          content="width=390, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+         />
         <meta name="format-detection" content="telephone=no,date=no,address=no,email=no" />
       </Head>
       <body>
