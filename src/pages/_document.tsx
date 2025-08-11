491392e4aee06181854564c0062b76adadef81e1
Jacob Knopf <jacobknopf@Jacobs-MacBook-Air.local>
2025-08-10 20:29:29 -0500

    Fixing Safari mobile scaling issue trying new CSS fix merging styles

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,9 +5,9 @@ export default function Document() {
     <Html lang="en">
       <Head>
         {/* Viewport meta tag to prevent zooming and font scaling on mobile Safari */}
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no"
+        <meta 
+          name="viewport" 
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover"
         />
         <meta name="format-detection" content="telephone=no,date=no,address=no,email=no" />
       </Head>
