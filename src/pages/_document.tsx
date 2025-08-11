b57b7630d690df8efccb0bd7e436dc3a39457d60
Jacob Knopf <jacobknopf@Jacobs-MacBook-Air.local>
2025-08-11 09:33:13 -0500

    Trying more comprehensive CSS fix targeting multiple text elements specifically for mobile Safari issue

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,8 +7,10 @@ export default function Document() {
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover"
-         />
+        />
         <meta name="format-detection" content="telephone=no,date=no,address=no,email=no" />
+        {/* Additional Safari-specific meta tag */}
+        <meta name="apple-mobile-web-app-capable" content="yes" />
       </Head>
       <body>
         <Main />
@@ -17,6 +19,7 @@ export default function Document() {
     </Html>
   )
 }
+
 
 
 
