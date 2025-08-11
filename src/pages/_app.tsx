9aca8e9b0399907c8daa9de27e92ebe7f4e65d5c
Jacob Knopf <jacobknopf@Jacobs-MacBook-Air.local>
2025-08-11 10:06:20 -0500

    Trying more comprehensive CSS fix targeting multiple text elements specifically for mobile Safari issue

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,7 @@ export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
     function setVhProperty() {
       const vh = window.innerHeight * 0.01
-      document.documentElement.style.setProperty('--vh', ${vh}px)
+      document.documentElement.style.setProperty('--vh', `${vh}px`)
     }
     setVhProperty()
     window.addEventListener('resize', setVhProperty)
