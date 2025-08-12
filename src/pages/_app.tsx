df2807bbd10d56fd6c39e3d74f58033c0f25c98d
Jacob Knopf <jacobknopf@Jacobs-MacBook-Air.local>
2025-08-12 10:33:33 -0500

    Hiding subpage nav titles and spacer reverting code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,11 +4,8 @@ import 'prismjs/themes/prism-tomorrow.css'
 import 'katex/dist/katex.min.css'
 import type { AppProps } from 'next/app'
 import { useEffect } from 'react'
-import { useRouter } from 'next/router'  // <-- import useRouter
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter()
-
   useEffect(() => {
     function setVhProperty() {
       const vh = window.innerHeight * 0.01
@@ -17,6 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
 
     setVhProperty()
 
+    // Listen to resize and scroll events to update --vh to fix phantom scroll on Chrome mobile
     window.addEventListener('resize', setVhProperty)
     window.addEventListener('scroll', setVhProperty)
 
@@ -26,20 +24,8 @@ export default function App({ Component, pageProps }: AppProps) {
     }
   }, [])
 
-  useEffect(() => {
-    // Add or remove 'home' class on body based on current path
-    if (typeof window !== 'undefined') {
-      if (router.pathname === '/') {
-        document.body.classList.add('home')
-      } else {
-        document.body.classList.remove('home')
-      }
-    }
-  }, [router.pathname])
-
   return <Component {...pageProps} />
 }
-
 
 
 
