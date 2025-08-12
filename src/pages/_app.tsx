6abb987fe65d356daa734fc9e7a4880b7c548b6a
Jacob Knopf <jacobknopf@Jacobs-MacBook-Air.local>
2025-08-11 19:56:19 -0500

    Fixing Chrome mobile footer issue again

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,12 +11,25 @@ export default function App({ Component, pageProps }: AppProps) {
       const vh = window.innerHeight * 0.01
       document.documentElement.style.setProperty('--vh', `${vh}px`)
     }
+
+    // Set on load
     setVhProperty()
+
+    // Update on resize, orientation change, and when returning to the page
     window.addEventListener('resize', setVhProperty)
-    return () => window.removeEventListener('resize', setVhProperty)
+    window.addEventListener('orientationchange', setVhProperty)
+    window.addEventListener('pageshow', setVhProperty)
+
+    return () => {
+      window.removeEventListener('resize', setVhProperty)
+      window.removeEventListener('orientationchange', setVhProperty)
+      window.removeEventListener('pageshow', setVhProperty)
+    }
   }, [])
+
   return <Component {...pageProps} />
 }
+
 
 
 
