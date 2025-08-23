47cef917f9ca00b7eb272d8e79b45f1915062252
Jacob Knopf <jacobknopf@Jacobs-MacBook-Air.local>
2025-08-22 22:00:19 -0500

    Trying adding alt text with file in data folder

--- src/data/altText.js
+++ src/data/altText.js
@@ -2,7 +2,7 @@
 
 const altText = {
   '/image/attachment%3A245490e8-44c9-4ec5-a4a9-ad7538d49ce9%3ANotion_Cover_Image_2_copy_7.png':
-    'Cover photo of Jacob Knopf swimming in the Sinclair Inlet"
+    'Cover photo of Jacob Knopf swimming in the Sinclair Inlet'
 }
 
 export default altText
