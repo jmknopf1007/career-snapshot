1ecd16757d464ba4703dd368de1bec9aa0f5dfb3
Jacob Knopf <jacobknopf@Jacobs-MacBook-Air.local>
2025-08-23 23:03:50 -0500

    Trying adding alt text with file in data folder

--- src/data/altText.js
+++ src/data/altText.js
@@ -2,7 +2,7 @@
 
 const altText = {
   // Cover photo
-  '/image/attachment%3A245490e8-44c9-4ec5-a4a9-ad7538d49ce9%3ANotion_Cover_Image_2_copy_7.png':
+  'https://www.notion.so/image/attachment%3A245490e8-44c9-4ec5-a4a9-ad7538d49ce9%3ANotion_Cover_Image_2_copy_7.png':
     'Cover photo of Jacob Knopf swimming in the Sinclair Inlet'
   
   // You can add more image paths here, e.g.:
