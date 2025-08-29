712f3d4c2c9584b68112c0c0e3dd34131f0b917a
Jacob Knopf <jacobknopf@Jacobs-MacBook-Air.local>
2025-08-28 21:40:57 -0500

    Trying to add alt text to bitmoji image

--- src/data/altText.js
+++ src/data/altText.js
@@ -1,18 +1,21 @@
 // src/data/altText.js
 
 const altText = {
+
   // Cover photo
   'https://www.notion.so/image/attachment%3A245490e8-44c9-4ec5-a4a9-ad7538d49ce9%3ANotion_Cover_Image_2_copy_7.png?table=block&amp;id=23b7fc8e-f6c2-8048-bc7b-e30a5325495c&amp;cache=v2':
-    'Cover photo of Jacob Knopf swimming in the Sinclair Inlet'
+    'Cover photo of Jacob Knopf swimming in the Sinclair Inlet',
 
-// Bitmoji photo
+  // Bitmoji photo
   'https://www.notion.so/image/attachment%3Aabafc4ae-e4df-40c2-8485-0923db243860%3Aimage0-removebg-preview_copy.png?table=block&amp;id=23b7fc8e-f6c2-817b-a50d-ca4d3d30fe39&amp;cache=v2':
-    'Bitmoji of Jacob Knopf!'
-  
+    'Bitmoji of Jacob Knopf!',
+
   // You can add more image paths here, e.g.:
   // '/image/other-image.png': 'Description of other image'
 }
 
 export default altText
+
+
 
 
