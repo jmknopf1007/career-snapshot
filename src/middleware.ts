6562525b26091cb0f825309bf4aa3218a9e42265
Jacob Knopf <jmknopf1007@gmail.com>
2026-06-30 19:23:27 -0500

    Adding password protection

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,31 +1,14 @@
-import { NextRequest, NextResponse } from 'next/server'
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
-  const auth = request.headers.get('authorization')
-
-  if (auth?.startsWith('Basic ')) {
-    const encoded = auth.split(' ')[1]
-    const decoded = atob(encoded)
-    const [user, password] = decoded.split(':')
-
-    if (
-      user === process.env.BASIC_AUTH_USER &&
-      password === process.env.BASIC_AUTH_PASSWORD
-    ) {
-      return NextResponse.next()
-    }
-  }
-
-  return new NextResponse('Authentication required.', {
-    status: 401,
-    headers: {
-      'WWW-Authenticate': 'Basic realm="Jacob Knopf Portfolio"',
-    },
+  return NextResponse.json({
+    user: process.env.BASIC_AUTH_USER ?? 'NOT SET',
+    password: process.env.BASIC_AUTH_PASSWORD ? 'SET' : 'NOT SET',
   })
 }
 
 export const config = {
-  matcher: [
-    '/((?!api|_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml|.*\\.(?:png|jpg|jpeg|gif|svg|webp|ico|css|js|woff2?|ttf|eot)$).*)',
-  ],
+  matcher: '/:path*',
 }
+
